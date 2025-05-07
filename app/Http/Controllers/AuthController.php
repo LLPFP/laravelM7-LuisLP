4cf0530ac69620ee6d701e211225fe6104d50cff
Luis López Puig <120658164+LLPFP@users.noreply.github.com>
2025-05-07 17:08:45 +0000

    Middleware para registro y login de usuarios api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        // Validate the request
+        $validator->validator::make($request->all(),[
+            'name' => 'required|string|max:100',
+            'role' => 'required|string|in:admin,user',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create the user
+        $user = User::create([
+            'name' => $request->get('name'),
+            'role' => $request->get('role'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+
+        // Return a success response
+        return response()->json(['message' => 'User registered successfully', 'data' => $user], 201);
+
+    }
+
+
+    public function login(Request $request)
+    {
+        // Validate the request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string|min:5',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+        // Attempt to log the user in
+        try{
+            if(!$token = JWTAuth::attempt($credentials)){
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+            return response()->json(['message' => 'Login successful', 'token' => $token], 200);
+
+        }catch (JWTException $e){
+            return response()->json
+            (['error' => 'Could not create token', 'message' => $e->getMessage()], 500);
+
+
+        }
+    }
+
+    public function getUser(){
+        // Get the authenticated user
+        $user = auth()->user();
+
+        return response()->json
+        ([
+        'message'=>'User retrieved succesfully',
+        'data' => $user], 200);
+    }
+
+
+    public function logout()
+    {
+        try{
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json(['message' => 'User logged out successfully'], 200);
+        }catch (JWTException $e){
+            return response()->json(['message' => 'Could not log out', 500]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,40 @@ student'; });
 
 Route::get('/students/{id}', function () { return 'Getting single
 student'; });
+
+
+
+
+
+
+
+
+
+// Public routes
+Route::post('/register', [AuthController::class,'register']);
+
+Route::post('/login', [AuthController::class,'login']);
+Route::get('/personajes', [AuthController::class,'getPersonajes']);
+Route::get('/personajes/{id}', [AuthController::class,'getPersonaje']);
+
+// Protected routes
+Route::middleware([IsUserAuth::class])->group(function () {
+    Route::post(('logout'), [AuthController::class,'logout']);
+    Route::get('me', [AuthController::class,'getUser']);
+});
+
+
+// Admin routes
+
+Route::middleware([IsAdmin::class])->group(function () {
+   Route::get('users', [AuthController::class,'getUsers']);
+    Route::get('users/{id}', [AuthController::class,'getUser']);
+    Route::put('users/{id}', [AuthController::class,'updateUser']);
+    Route::delete('users/{id}', [AuthController::class,'deleteUser']);
+
+    Route::post('personajes', [AuthController::class,'addPersonaje']);
+    Route::put('personajes/{id}', [AuthController::class,'updatePersonaje']);
+    Route::delete('personajes/{id}', [AuthController::class,'deletePersonaje']);
+    Route::get('personajes', [AuthController::class,'getPersonajes']);
+    Route::get('personajes/{id}', [AuthController::class,'getPersonaje']);
+});
